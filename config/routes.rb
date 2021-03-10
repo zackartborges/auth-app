82347bb5d4ea32216f4cce12d5ff9f0500a9797d
Zack Borges <zackartborges@gmail.com>
2021-03-10 10:03:29 -0500

    adds authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    # get "/photos" => "photos#index"
+    post "/users" => "users#create"
+    post "/sessions" => "sessions#create"
+  end
 end
